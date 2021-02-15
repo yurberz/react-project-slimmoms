b3109ac2654ff333c57b0eb837d34b37fa82116e
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-02-15 15:04:43 +0200

    update

--- src/components/layout/LayoutStyled.js
+++ src/components/layout/LayoutStyled.js
@@ -1,19 +1,20 @@
 import styled from "styled-components";
 
 const Div = styled.div`
-  max-width: 320px;
+  min-width: 320px;
+  width: 100%;
   padding-left: 15px;
   padding-right: 15px;
   margin: 0 auto;
 
-  @media (min-width: 768px) {
-    min-width: 768px;
+  @media screen and (min-width: 768px) {
+    width: 768px;
     padding-left: 86px;
     padding-right: 86px;
   }
 
-  @media (min-width: 1280px) {
-    min-width: 1280px;
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
     padding-left: 115px;
     padding-right: 115px;
   }
