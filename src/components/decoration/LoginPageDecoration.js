5e5cde46e0f276e40aae60736e7893ec4b5a74ae
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-02-17 12:58:50 +0200

    div

--- src/components/decoration/LoginPageDecoration.js
+++ src/components/decoration/LoginPageDecoration.js
@@ -3,12 +3,12 @@ import s from './LoginPageDecoration.module.css'
 
 const LoginPageDecoration = () => {
     return (
-        <section className={s.decoration}>
+        <div className={s.decoration}>
             <div className={s.decspot}></div>
             <div className={s.decstrawberry}></div>
             <div className={s.decleaves}></div>
             <div className={s.decbanana}></div>
-        </section>
+        </div>
     );
 }
 
