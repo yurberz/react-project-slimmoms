3e2788160bd6e88777b5346da4c0d2eda99fef9c
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-02-18 11:57:19 +0200

    up

--- src/utils/rightSideBar/rightSideBar.js
+++ src/utils/rightSideBar/rightSideBar.js
@@ -16,48 +16,46 @@ class RightSideBar extends Component {
     // const { date, left, used, norm, prcnt } = this.state;
 
     return (
-      <>
-        <Section>
-          <div className="dayInfoWrapper">
-            <h2>Сводка за 17.02.2021</h2>
-            <ul>
-              <li>
-                <p>
-                  Осталось
-                  <span>000 ккал</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  Употреблено
-                  <span>000 ккал</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  Дневная норма
-                  <span>000 ккал</span>
-                </p>
-              </li>
-              <li>
-                <p>
-                  n% от нормы
-                  <span>000 ккал</span>
-                </p>
-              </li>
-            </ul>
-          </div>
-          <div className="notAllowedWrapper">
-            <h2>Нерекомендуемые продукты</h2>
-            <div>
+      <Section>
+        <div className="dayInfoWrapper">
+          <h2>Сводка за 17.02.2021</h2>
+          <ul>
+            <li>
               <p>
-                Apple, Carrot, Pineapple, Apple, Carrot, Pineapple,Apple,
-                Carrot, Pineapple
+                Осталось
+                <span>000 ккал</span>
               </p>
-            </div>
+            </li>
+            <li>
+              <p>
+                Употреблено
+                <span>000 ккал</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                Дневная норма
+                <span>000 ккал</span>
+              </p>
+            </li>
+            <li>
+              <p>
+                n% от нормы
+                <span>000 ккал</span>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="notAllowedWrapper">
+          <h2>Нерекомендуемые продукты</h2>
+          <div>
+            <p>
+              Apple, Carrot, Pineapple, Apple, Carrot, Pineapple,Apple, Carrot,
+              Pineapple
+            </p>
           </div>
-        </Section>
-      </>
+        </div>
+      </Section>
     );
   }
 }
