175ef1c6b29e5d0badd4955c9bfd03baf80b111d
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-02-19 22:11:02 +0200

    переробляю стилі на компоненти

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -2,17 +2,19 @@ import { Component } from "react";
 import DiaryAddProduct from "../components/Diary/DiaryAddProduct";
 import DiaryCalendar from "../components/Diary/DiaryCalendar";
 import DiaryList from "../components/Diary/DiaryList";
-import DiaryStyle from "../components/Diary/DiaryStyled"
+import DiaryStyle from "../components/Diary/DiaryStyled";
+import CalculatorPageDecoration from '../components/decoration/CalculatorPageDecoration';
 
 class Diary extends Component {
-  
+
     render() {
-    return(
-        <DiaryStyle>
-            <DiaryCalendar /> 
-            <DiaryAddProduct />
-            <DiaryList/>
-        </DiaryStyle>
+        return (
+            <DiaryStyle>
+                <DiaryCalendar />
+                <DiaryAddProduct />
+                <DiaryList />
+                <CalculatorPageDecoration />
+            </DiaryStyle>
         );
     }
 }
