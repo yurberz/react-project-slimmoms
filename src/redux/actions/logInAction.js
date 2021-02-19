9bec1e40e3d20f5e451cf9fba6e26647827831da
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-02-19 20:58:26 +0200

    fix logInAcion(dublicate const chngParams)

--- src/redux/actions/logInAction.js
+++ src/redux/actions/logInAction.js
@@ -13,8 +13,6 @@ export const newTknRequest = createAction("logIn/newTknRequest");
 export const newTknSuccess = createAction("logIn/newTknSuccess");
 export const newTknError = createAction("logIn/newTknError");
 
-export const chngParams = createAction("@CHNG_USER_PARAMS");
 export const chngRecomendsRequest = createAction("@CHNG_RECOMENDS_REQUEST");
 export const chngRecomendsSuccess = createAction("@CHNG_RECOMENDS_SUCCESS");
 export const chngRecomendseError = createAction("@CHNG_RECOMENDS_ERROR");
-
