911efe197f9f7f06a8d68f80bea99ebe1488569a
Yurii Berezynets <yuriiberezynets@gmail.com>
2021-02-20 15:42:57 +0200

    update css for CalcPage

--- src/utils/rightSideBar/rightSideBar.js
+++ src/utils/rightSideBar/rightSideBar.js
@@ -6,62 +6,91 @@ import moment from "moment";
 
 class RightSideBar extends Component {
   render() {
-    const { daySummary, notAllowedProducts } = this.props;
+    const { daySummary, notAllowedProducts, dailyRate } = this.props;
 
     return (
       <Section>
         <div className="dayInfoWrapper">
           <h2>Сводка за {moment(daySummary.date).format("DD.MM.YYYY")}</h2>
-          <ul>
-            <li>
-              <p>
-                Осталось
-                <span>
-                  {daySummary.kcalLeft
-                    ? (daySummary.kcalConsumed > daySummary.dailyRate
-                        ? 0
-                        : Math.round(daySummary.kcalLeft)) + " ккал"
-                    : "000 калл"}
-                </span>
-              </p>
-            </li>
-            <li>
-              <p>
-                Употреблено
-                <span>
-                  {daySummary.kcalConsumed
-                    ? Math.round(daySummary.kcalConsumed) + " ккал"
-                    : "000 калл"}
-                </span>
-              </p>
-            </li>
-            <li>
-              <p>
-                Дневная норма
-                <span>
-                  {daySummary.dailyRate
-                    ? Math.round(daySummary.dailyRate) + " ккал"
-                    : "000 калл"}
-                </span>
-              </p>
-            </li>
-            <li>
-              <p>
-                n% от нормы
-                <span>
-                  {daySummary.percentsOfDailyRate
-                    ? Math.round(daySummary.percentsOfDailyRate) + " %"
-                    : "000 калл"}
-                </span>
-              </p>
-            </li>
-          </ul>
+          {daySummary ? (
+            <ul>
+              <li>
+                <p>
+                  Осталось
+                  <span>
+                    {daySummary.kcalLeft
+                      ? (daySummary.kcalConsumed > daySummary.dailyRate
+                          ? 0
+                          : Math.round(daySummary.kcalLeft)) + " ккал"
+                      : "000 калл"}
+                  </span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Употреблено
+                  <span>
+                    {daySummary.kcalConsumed
+                      ? Math.round(daySummary.kcalConsumed) + " ккал"
+                      : "000 калл"}
+                  </span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Дневная норма
+                  <span>
+                    {dailyRate ? Math.round(dailyRate) + " ккал" : "000 калл"}
+                  </span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  n% от нормы
+                  <span>
+                    {daySummary.percentsOfDailyRate
+                      ? Math.round(daySummary.percentsOfDailyRate) + " %"
+                      : "000 калл"}
+                  </span>
+                </p>
+              </li>
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                <p>
+                  Осталось
+                  <span>000 калл</span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Употреблено
+                  <span>000 калл</span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  Дневная норма
+                  <span>
+                    {dailyRate ? Math.round(dailyRate) + " ккал" : "000 калл"}
+                  </span>
+                </p>
+              </li>
+              <li>
+                <p>
+                  n% от нормы
+                  <span>000 калл}</span>
+                </p>
+              </li>
+            </ul>
+          )}
         </div>
         <div className="notAllowedWrapper">
           <h2>Нерекомендуемые продукты</h2>
 
           {!!notAllowedProducts ? (
-            <div className="notAllowedProductsList">
+            <div className="notAllowedProducts">
               {notAllowedProducts.map((product) => (
                 <p key={product}>{product}, </p>
               ))}
@@ -78,6 +107,7 @@ class RightSideBar extends Component {
 const mSTP = (state) => ({
   daySummary: getUserInfoSelectors.getDaySummary(state),
   notAllowedProducts: getUserInfoSelectors.getNotAllowrdProducts(state),
+  dailyRate: getUserInfoSelectors.getDailyRate(state),
 });
 
 export default connect(mSTP)(RightSideBar);
