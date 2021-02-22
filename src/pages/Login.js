44e205d1aa2c5c99c1ed653139fab9aa9594cd44
Your Name <stasgill@gmail.com>
2021-02-22 14:30:37 +0200

    new fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,8 +13,17 @@ import "../components/form/register.css";
 const ContainerForm = styled.div`
   max-width: 439px;
   padding: 15px;
+  @media screen and (max-width: 1279px) {
+    margin: 82px auto;
+  }
   @media screen and (max-width: 768px) {
-    margin: 0 auto;
+    margin: 82px auto 82px auto;
+  }
+  @media screen and (max-width: 768px) {
+    margin: 82px auto 82px auto;
+  }
+  @media screen and (max-width: 390px) {
+    margin: 20px auto 0px auto;
   }
 `;
 const ContainerButton = styled.div`
@@ -23,6 +32,10 @@ const ContainerButton = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
   padding: 0px;
+  margin-top: 82px;
+  @media screen and (max-width: 390px) {
+    margin-top: 42px;
+  }
 `;
 
 const HeadingH1 = styled.h1`
@@ -34,9 +47,14 @@ const HeadingH1 = styled.h1`
   line-height: 13px;
   letter-spacing: 0.04em;
   margin-bottom: 60px;
-  padding-top: 95px;
   @media screen and (min-width: 1300px) {
     padding-top: 0;
+    margin-top: 0;
+  }
+  @media screen and (max-width: 766px) {
+    padding-top: 0;
+    margin-top: 0;
+    text-align: center;
   }
 `;
 
@@ -44,29 +62,52 @@ class Login extends Component {
   state = {
     email: "",
     password: "",
+    error: "",
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.logInOperation(this.state);
-    this.setState({ email: "", password: "" });
+    if (!this.state.email.includes("@")) {
+      this.setState({
+        error: "Адрес почты введен неверно",
+      });
+      return;
+    } else if (
+      this.state.password.length < 3 ||
+      this.state.password.length > 12
+    ) {
+      this.setState({
+        error: "Пароль должно содержать не меньше 3 символов и не больше 12",
+      });
+      return;
+    } else {
+      this.props.logInOperation({
+        email: this.state.email,
+        password: this.state.password,
+      });
+      this.setState({ email: "", password: "", error: "" });
+    }
   };
 
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  // logOutt = (e) => {
-  //   e.preventDefault();
-
-  //   this.props.logOut();
-  // };
-
   render() {
     const error = this.props.error;
     return (
       <ContainerForm>
         <LoginPageDecoration />
+        <CSSTransition
+          in={this.state.error.length >= 1}
+          timeout={500}
+          classNames="fade"
+          unmountOnExit
+        >
+          <div className="warning">
+            <p>{this.state.error}</p>
+          </div>
+        </CSSTransition>
         <CSSTransition
           in={error.length >= 1}
           timeout={500}
@@ -88,7 +129,7 @@ class Login extends Component {
           />
 
           <Inputt
-            text={""}
+            text={"password"}
             placeholder={"Пароль *"}
             name={"password"}
             onChange={this.onChange}
@@ -96,10 +137,9 @@ class Login extends Component {
           />
           <ContainerButton>
             <Buttonn type={"submit"} text={"Вход"} />
-            <Link to={{ pathname: "/registration" }}>
+            <Link to={{ pathname: "/registration" }} className="linkTablet">
               <Buttonn type={""} text={"Регистрация"} />
             </Link>
-            {/* <button onClick={this.logOutt}>Выйти</button> */}
           </ContainerButton>
         </form>
       </ContainerForm>
